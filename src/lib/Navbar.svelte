1ce21347e21ab1ab957666fd0c9119cb9ad4e969
RaczAkos <akocska2005@gmail.com>
2024-11-10 22:46:40 +0100

    navbar_half

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,23 @@
+<script>
+	import logo from '$lib/media/images/logo.png';
+</script>
+
+<nav>
+    <div class="justify-center w-screen">
+            <div class="navsec inline-block text-white">
+                <a href="/conditions"><h1>Terms & conditions</h1></a>
+            </div>
+            <div class="navsec inline-block text-white">
+                <a href="/policy"><h1>Privacy Policy</h1></a>
+            </div>
+        <div class="navone inline-block mx-auto">
+            <img src="{logo}" alt="">
+        </div>
+        <div class="navsec inline-block text-white">
+            <a href="/login"><h1>Log in</h1></a>
+        </div>
+        <div class="navsec inline-block text-white right-0">
+            <a href="/signUp"><h1>Sign up</h1></a>
+        </div>
+    </div>
+</nav>
