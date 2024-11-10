ed697b6226f93fbbcde9b9f8112fe694e98a7fea
RaczAkos <akocska2005@gmail.com>
2024-11-10 23:28:41 +0100

    navbar_braindead

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -3,21 +3,25 @@
 </script>
 
 <nav>
-    <div class="justify-center w-screen">
-            <div class="navsec inline-block text-white">
+    <div class="justify-center w-screen relative">
+        <div class="navone absolute">
+            <div class="navsec inline-block text-white border rounded-md hover:bg-yellow-600 hover:text-black hover:border-yellow-600">
                 <a href="/conditions"><h1>Terms & conditions</h1></a>
             </div>
-            <div class="navsec inline-block text-white">
+            <div class="navsec inline-block text-white border rounded-md hover:bg-yellow-600 hover:text-black hover:border-yellow-600">
                 <a href="/policy"><h1>Privacy Policy</h1></a>
             </div>
-        <div class="navone inline-block mx-auto">
+        </div>
+        <div class="flex flex-row justify-center items-center">
             <img src="{logo}" alt="">
         </div>
-        <div class="navsec inline-block text-white">
-            <a href="/login"><h1>Log in</h1></a>
-        </div>
-        <div class="navsec inline-block text-white right-0">
-            <a href="/signUp"><h1>Sign up</h1></a>
+        <div class="navone absolute inset-y-0 right-0 align-middle">
+            <div class="navsec inline-block text-white border rounded-md hover:bg-yellow-600 hover:text-black hover:border-yellow-600">
+                <a href="/login"><h1>Log in</h1></a>
+            </div>
+            <div class="navsec inline-block border rounded-md bg-yellow-600 text-black border-yellow-600">
+                <a href="/signUp"><h1>Sign up</h1></a>
+            </div>
         </div>
     </div>
 </nav>
