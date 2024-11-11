9ecc53792c98a4bda6209dd1d59cdf0bfb85aa6a
RaczAkos <akocska2005@gmail.com>
2024-11-11 14:25:36 +0100

    navbar_almost done

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -3,23 +3,20 @@
 </script>
 
 <nav>
-    <div class="justify-center w-screen relative">
-        <div class="navone absolute">
-            <div class="navsec inline-block text-white border rounded-md hover:bg-yellow-600 hover:text-black hover:border-yellow-600">
-                <a href="/conditions"><h1>Terms & conditions</h1></a>
-            </div>
-            <div class="navsec inline-block text-white border rounded-md hover:bg-yellow-600 hover:text-black hover:border-yellow-600">
-                <a href="/policy"><h1>Privacy Policy</h1></a>
+    <div class="w-screen flex justify-between ">
+        <div class="place-content-center w-1/3">
+            <div class="inline-block text-2xl p-2 shadow-lg shadow-yellow-600 text-white border rounded-md hover:bg-yellow-600 hover:text-black hover:border-yellow-600">
+                <a href="/conditions"><h1>Casino rules</h1></a>
             </div>
         </div>
-        <div class="flex flex-row justify-center items-center">
+        <div class="">
             <img src="{logo}" alt="">
         </div>
-        <div class="navone absolute inset-y-0 right-0 align-middle">
-            <div class="navsec inline-block text-white border rounded-md hover:bg-yellow-600 hover:text-black hover:border-yellow-600">
+        <div class="place-content-center w-1/3 ">
+            <div class="inline-block text-2xl p-2 shadow-lg shadow-yellow-600 text-white border rounded-md hover:bg-yellow-600 hover:text-black hover:border-yellow-600 mr-5">
                 <a href="/login"><h1>Log in</h1></a>
             </div>
-            <div class="navsec inline-block border rounded-md bg-yellow-600 text-black border-yellow-600">
+            <div class="inline-block text-2xl p-2 border rounded-md bg-yellow-600 text-black border-yellow-600 mr-5">
                 <a href="/signUp"><h1>Sign up</h1></a>
             </div>
         </div>
