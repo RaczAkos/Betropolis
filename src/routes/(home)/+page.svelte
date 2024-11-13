a2385c90f1f0cab5f29836187af772c8fe23a88e
SzaboTamasKeri <szabo.tamas-2020+@keri.mako.hu>
2024-11-13 14:29:03 +0100

    merge

--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -4,11 +4,7 @@
 
 </script>
 
-<<<<<<< HEAD
 <header class="w-[1440px] xl:w-screen">
-=======
-<header class="md:w-[1920px]">
->>>>>>> 8d5e8a9f35495c2ffcde52d59b13fa2fe4be90eb
     <video  src={banner} autoplay muted loop class=" blur-sm drop-shadow-none sepia-0">
     <div class="overlay">
         <p>asd</p>
