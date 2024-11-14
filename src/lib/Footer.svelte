8bb7967ba337b50d4b60c922852545c6bb9fc44a
SzaboTamasKeri <szabo.tamas-2020+@keri.mako.hu>
2024-11-14 12:01:13 +0100

    Footer update

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -2,29 +2,25 @@
     import logo from "$lib/media/images/logo.png";
 </script>
 
-<footer class="bg-white shadow dark:bg-gray-900">
-    <div class="w-full mx-auto md:py-8 p-4">
-        <div class="sm:flex sm:items-center sm:justify-between">
-            <a href="" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                <img src={logo} class="h-8" alt="Casino logo" />
+<footer class="bg-transparent">
+    <div class="w-full mx-auto md:py-4 p-3 font-[Casino3DFilled] text-yellow-600">
+        <div class="sm:flex sm:items-center sm:justify-between md:ps-9">
+            <a href="" class="flex items-center max-md:flex-col mb-0 space-x-3 rtl:space-x-reverse">
+                <img src={logo} class="max-md:h-32 h-24" alt="Casino logo" />
             </a>
-            <ul id='alma' class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+            <ul id='alma' class="flex flex-wrap max-md:flex-col items-center text-xl font-medium">
                 <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">About</a>
+                    <a href="/about" class="hover:underline sm:me-6">About</a>
                 </li>
                 <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Terms & Conditions</a>
+                    <a href="/terms&conditions" class="hover:underline sm:me-6">Terms & Conditions</a>
                 </li>
                 <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline">Contact</a>
+                    <a href="/privacypolicy" class="hover:underline sm:me-6">Privacy Policy</a>
                 </li>
             </ul>
         </div>
-        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 Betropolis™. All Rights Reserved.</span>
+        <hr class="sm:mx-auto border-yellow-600 my-4"/>
+        <span class="block text-md  text-center">© 2024-2025 Betropolis™. All Rights Reserved.</span>
     </div>
 </footer>
-
