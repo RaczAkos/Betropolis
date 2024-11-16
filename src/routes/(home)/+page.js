a898e07744e4c32c6af5f03fad03b2c6c127ffb0
RaczAkos <akocska2005@gmail.com>
2024-11-16 17:13:25 +0100

    modal_progress

--- src/routes/(home)/+page.js
+++ src/routes/(home)/+page.js
@@ -4,8 +4,8 @@ import diamond from "$lib/media/images/diamond.png"
 import heart from "$lib/media/images/heart.png"
 import spade from "$lib/media/images/spades.png"
 if (browser) {
-    let tbody = document.querySelector("tbody");
-    let tr = document.createElement("tr");
+    let tbody = document.querySelector("tbody"),
+    tr = document.createElement("tr");
     for (let i = 0; i < 4; i++) {
         let td = document.createElement("td");
         td.classList.add(
@@ -30,12 +30,12 @@ if (browser) {
         td.addEventListener("click", reveal);
         tr.classList.add("ledBorder");
         tr.append(td);
-        tr.classList.add("grid", "grid-cols-4", "gap-2", "max-sm:grid-cols-2", "max-sm:");
+        tr.classList.add("grid", "grid-cols-4", "gap-2", "max-sm:grid-cols-2");
     }
-    tbody?.append(tr);
-    let cards = document.querySelectorAll("tbody tr td");
 
-    let symbols = [
+    tbody?.append(tr);
+    let cards = document.querySelectorAll("tbody tr td:not(.modalImg)"),
+    symbols = [
         club, diamond, heart, spade
     ];    
     symbols.sort( () => .5 - Math.random() );
@@ -44,7 +44,39 @@ if (browser) {
         cards[i].innerHTML = "<img src='" + symbols[i] + "' class='h-1/2 w-auto'>";
     }
 
+
+    let   modal = document.getElementById("prize"),
+    modalCancel = document.getElementById("cancel");
+    modalCancel?.addEventListener("click", closeModal);
+
     function reveal(e) {
         let target = e.target;
+        target.classList.add("chosen");
+        let no = document.querySelectorAll("tbody tr td:not(.chosen)").forEach(element => {
+            element.removeEventListener("click", reveal);
+        });
+        modal?.classList.remove("hidden");
     }
+
+    function closeModal() {
+        modal?.classList.add("hidden");
+    }
+
+    let modalImgs = document.querySelectorAll("td.modalImg");
+    modalImgs[0].parentElement?.classList.add("grid", "grid-cols-4", "gap-2", "max-sm:grid-cols-2");
+    modalImgs[0].parentElement?.parentElement?.classList.add("ledBorder");
+    modalImgs[0].parentElement?.parentElement?.parentElement?.classList.add("w-full");
+    modalImgs.forEach(element => {
+        element.classList.add(
+            "space-x-3",
+            "h-20", 
+            "bg-zinc-950",
+            "rounded-xl",
+            "flex",
+            "items-center",
+            "justify-center",
+            "transition-all"
+        );
+        element.classList.add("w-auto");
+    });
 }
