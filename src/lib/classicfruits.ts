a2bbb6f5addf5f2e6c93d11b9060dbfa7825d714
SzaboTamasKeri <szabo.tamas-2020+@keri.mako.hu>
2024-11-19 14:07:57 +0100

    Slot machine progress, icons for slots, library reordering

---
+++ src/lib/classicfruits.ts
@@ -0,0 +1,12 @@
+import apple from "$lib/media/images/sloticons/apple.png";
+import bell from "$lib/media/images/sloticons/bell.png";
+import cherry from "$lib/media/images/sloticons/cherry.png";
+import orange from "$lib/media/images/sloticons/orange.png";
+import plum from "$lib/media/images/sloticons/plum.png";
+import strawberry from "$lib/media/images/sloticons/strawberry.png";
+import watermelon from "$lib/media/images/sloticons/watermelon.png";
+import lemon from "$lib/media/images/sloticons/lemon.png";
+
+let icons:string[] = [apple, bell, cherry, plum, orange, strawberry, watermelon, lemon];
+
+export default icons;
--- src/lib/frenchcards.ts
+++ src/lib/frenchcards.ts
@@ -1,7 +1,7 @@
-import clubs from "$lib/media/images/clubs.png";
-import diamond from "$lib/media/images/diamond.png";
-import spades from "$lib/media/images/spades.png";
-import heart from "$lib/media/images/heart.png";
+import clubs from "$lib/media/images/frenchcards/clubs.png";
+import diamond from "$lib/media/images/frenchcards/diamond.png";
+import spades from "$lib/media/images/frenchcards/spades.png";
+import heart from "$lib/media/images/frenchcards/heart.png";
 
 let frenchcards: string[] = [clubs, diamond, spades, heart];
 export default frenchcards;
