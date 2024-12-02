7e284ce88464e8273293b863766d16ddf2c8dc81
SzaboTamasKeri <szabo.tamas-2020+@keri.mako.hu>
2024-12-02 14:22:41 +0100

    slot progress, gold icons

--- src/lib/classicfruits.ts
+++ src/lib/classicfruits.ts
@@ -1,12 +1,19 @@
 import apple from "$lib/media/images/sloticons/apple.png";
-import goldencherry from "$lib/media/images/sloticons/goldencherry.png";
 import cherry from "$lib/media/images/sloticons/cherry.png";
 import orange from "$lib/media/images/sloticons/orange.png";
 import plum from "$lib/media/images/sloticons/plum.png";
 import strawberry from "$lib/media/images/sloticons/strawberry.png";
 import watermelon from "$lib/media/images/sloticons/watermelon.png";
 import lemon from "$lib/media/images/sloticons/lemon.png";
+import goldenapple from "$lib/media/images/sloticons/goldenapple.png";
+import goldenorange from "$lib/media/images/sloticons/goldenorange.png";
+import goldenplum from "$lib/media/images/sloticons/goldenplum.png";
+import goldenstrawberry from "$lib/media/images/sloticons/goldenstrawberry.png";
+import goldenwatermelon from "$lib/media/images/sloticons/goldenwatermelon.png";
+import goldenlemon from "$lib/media/images/sloticons/goldenlemon.png";
+import goldencherry from "$lib/media/images/sloticons/goldencherry.png";
 
-let icons:string[] = [apple, cherry, plum, orange, strawberry, goldencherry, watermelon, lemon];
+let goldenicons:string[] = [goldenapple, goldencherry, goldenplum, goldenorange, goldenstrawberry, goldenwatermelon, goldenlemon] 
+let icons:string[] = [apple, cherry, plum, orange, strawberry, watermelon, lemon, goldenicons[Math.floor(Math.random() * goldenicons.length)]];
 
 export default icons;
--- src/routes/slotmachine/+page.svelte
+++ src/routes/slotmachine/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import icons from "$lib/classicfruits";
-    import { onMount } from "svelte";
 
     let balance = $state(1000);
     let slot1:string = icons[giveRandom(icons)],
@@ -18,7 +17,7 @@
     function spin() {
         
         balance -= bet;
-        feedback = "Spinning!"
+        feedback = "Spinning!";
         spinning = true;
 
         let index1: number = giveRandom(icons),
@@ -103,28 +102,45 @@
         let fruit1:string = fruits[0].split('/')[fruits[0].split('/').length-1],
             fruit2:string = fruits[1].split('/')[fruits[1].split('/').length-1],
             fruit3:string = fruits[2].split('/')[fruits[2].split('/').length-1],
-            quips:string[] = ["Not this time!", "No luck!"],
+            quips:string[] = ["Not this time!", "No luck!", "Try again!", "Almost!", "Maybe next time!"],
             gain:number = 0;
 
         fruit1 = fruit1.replace(fruit1.substring(fruit1.length-4), "");
         fruit2 = fruit2.replace(fruit2.substring(fruit2.length-4), "");
         fruit3 = fruit3.replace(fruit3.substring(fruit3.length-4), "");
         
-        
-        if (false) {
-            // gold fruit 
+        feedback = "";
+        if (fruit1.indexOf("golden") != -1 && fruit2.indexOf("golden") != -1 && fruit3.indexOf("golden") != -1) {
+            if (!autorun) output("GOLDEN FRUIT FRENZY!!!", 4);
+            else feedback = "GOLDEN FRUIT FRENZY!!!";
+            gain = 500 * bet;
+        }
+        else if (fruit1.indexOf("golden") != -1 && fruit2.indexOf("golden") != -1) {
+            if (!autorun) output("Double Golden Fruit!!!", 3);
+            else feedback = "Double Golden Fruit!!!";
+            gain = 10 * bet;
+        }
+        else if (fruit1.indexOf("golden") != -1 && fruit3.indexOf("golden") != -1) {
+            if (!autorun) output("Double Golden Fruit!!!", 3);
+            else feedback = "Double Golden Fruit!!!";
+            gain = 10 * bet;
+        }
+        else if (fruit2.indexOf("golden") != -1 && fruit3.indexOf("golden") != -1) {
+            if (!autorun) output("Double Golden Fruit!!!", 3);
+            else feedback = "Double Golden Fruit!!!";            
+            gain = 10 * bet;
         }
         else if (fruit1 == fruit2 && fruit1 == fruit3) {
-            if (!autorun) output("Tripple Fruit!!!", 3);
-            else feedback = "Tripple Fruit!!!";
+            if (!autorun) output("FRUIT FRENZY!!!", 3);
+            else feedback = "FRUIT FRENZY!!!";
             gain = 100 * bet;
         }
         else if (fruit1 == fruit2 || fruit1 == fruit3 || fruit2 == fruit3) {
-            if (!autorun) output("Double Fruit!!", 2);
+            if (!autorun) output("Double Fruit!!", 1);
             else feedback = "Double Fruit!!";
             gain = 3 * bet;
         }
-        else output(quips[giveRandom(quips)], 1);
+        else feedback = quips[giveRandom(quips)];
 
         // transaction
 
@@ -135,35 +151,49 @@
 
     function output(feed:string, type:number) {
         switch (type) {
-            case 3:
-                let int3 = setInterval(() => {
-                        feedback = feed;
-                        setTimeout(() => {
-                            feedback = "";
-                        }, 250)
-
-                        setTimeout(() => {
-                            clearInterval(int3);
-                            feedback = feed;
-                            check();
-                        }, 2000)
-                    }, 500);
-                    break;
-            case 1:
-                let int1 = setInterval(() => {
+            case 4:
+                let int4 = setInterval(() => {
                     feedback = feed;
                     setTimeout(() => {
                         feedback = "";
-                    }, 500)
-
+                    }, 50);
+                    
                     setTimeout(() => {
-                        clearInterval(int1);
+                        clearInterval(int4);
                         feedback = feed;
                         check();
-                    }, 2000)
-                }, 1000);
+                    }, 1000)
+                }, 100);
                 break;
-        
+            case 3:
+            case 2:
+                let int3 = setInterval(() => {
+                    feedback = feed;
+                    setTimeout(() => {
+                        feedback = "";
+                    }, 100);
+                    
+                    setTimeout(() => {
+                        clearInterval(int3);
+                        feedback = feed;
+                        check();
+                    }, 1000)
+                }, 200);
+                break;
+            case 1:
+                let int2 = setInterval(() => {
+                    feedback = feed;
+                    setTimeout(() => {
+                        feedback = "";
+                    }, 250);
+
+                    setTimeout(() => {
+                        clearInterval(int2);
+                        feedback = feed;
+                        check();
+                    }, 1000)
+                }, 500);
+                break;
             default:
                 break;
         }
@@ -178,7 +208,7 @@
 <div class="h-screen flex justify-center items-center text-white select-none background overflow-hidden">
     <div class="border p-2 bg-red-600 rounded-3xl">
         <div>
-            <h1 class="text-5xl text-center orangek">Fruit Frenzy</h1>
+            <h1 class="text-5xl text-center orangek font-bold">Fruit Frenzy</h1>
         </div>
         <div class="border flex flex-row items-center mb-2">
             {#each [slot1, slot2, slot3] as item,i}
@@ -189,10 +219,10 @@
         </div>
         <div class="flex flex-col gap-2">
             <input type="text" disabled class="p-2 rounded-2xl sm:hidden text-center" bind:value={feedback}>
-            <div class="flex flex-row gap-2">
-                <button type="button" class="border basis-1/4 p-2 rounded-2xl max-sm:basis-1/2 disabled:bg-black" disabled={spinning || !bet || bet < 0 || balance < bet} onclick={spin}>{#if spinning}Spinning{:else}Spin{/if}</button>
-                <input type="text" disabled class="basis-2/4 p-2 rounded-2xl max-sm:hidden text-center" bind:value={feedback}>
-                <button type="button" class=" border basis-1/4 p-2 rounded-2xl hover:animate-pulse animation max-sm:basis-1/2" disabled={!bet || bet < 0 || balance < bet} onclick={() => {autorun = !autorun; if (!spinning) spin();}}>{#if autorun}Stop Autorun{:else}Autorun{/if}</button>
+            <div class="flex flex-row gap-2 orangek">
+                <button type="button" class="border basis-1/4 p-1 text-lg rounded-2xl max-sm:basis-1/2 disabled:bg-black" disabled={spinning || !bet || bet < 0 || balance < bet} onclick={spin}>Spin</button>
+                <input type="text" disabled class="basis-2/4 p-1 text-lg rounded-2xl max-sm:hidden text-center" bind:value={feedback}>
+                <button type="button" class=" border basis-1/4 p-1 text-lg rounded-2xl hover:animate-pulse animation max-sm:basis-1/2" disabled={!bet || bet < 0 || balance < bet} onclick={() => {autorun = !autorun; if (!spinning) spin();}}>{#if autorun}Stop Autorun{:else}Autorun{/if}</button>
             </div>
             <div class="flex flex-row max-md:flex-col gap-1">
                 <div class="flex basis-1/2">
