71ab7ed261fcfb71564a80153fff8c45cd938bf2
SzaboTamasKeri <szabo.tamas-2020+@keri.mako.hu>
2024-12-03 11:33:35 +0100

    slot progress

--- src/routes/fruit-frenzy-classic/+page.svelte
+++ src/routes/fruit-frenzy-classic/+page.svelte
@@ -1,21 +1,20 @@
+<svelte:head>
+    <title>Fruit Frenzy Classic! - Betropolis</title>
+</svelte:head>
+
 <script lang="ts">
     import icons from "$lib/classicfruits";
-    import chip from "$lib/media//images/chip.png";
-    import { onMount } from "svelte";
+    import chip from "$lib/media/images/chip.png";
 
     let balance = $state(10000);
     let slot1:string = icons[giveRandom(icons)],
         slot2:string = icons[giveRandom(icons)],
         slot3:string = icons[giveRandom(icons)],
-        feedback:string = $state(""), 
+        feedback:string = $state("Start spinning!"), 
         bet:number = $state(1),
         autorun:boolean = $state(false),
-        spinning:boolean = $state(false);
-
-    onMount(() => {
-        feedback = "Start spinning!";
-        check();
-    })
+        spinning:boolean = $state(false),
+        modal:boolean = $state(false);
 
     function giveRandom(array:string[]) {
         return Math.floor(Math.random() * array.length);
@@ -50,7 +49,7 @@
             img.classList.add("absolute", "anim");
 
             if (i1 == 0){
-                clearInterval(int1)
+                clearInterval(int1);
                 img.classList.remove("anim");
                 fruits.push(icons[index1]);
             } 
@@ -72,7 +71,7 @@
                 img.classList.add("absolute", "anim");
 
                 if (i2 == 0){
-                    clearInterval(int2)
+                    clearInterval(int2);
                     img.classList.remove("anim");
                     fruits.push(icons[index2]);
                 } 
@@ -95,7 +94,7 @@
                 img.classList.add("absolute", "anim");
 
                 if (i3 == 0){
-                    clearInterval(int3)
+                    clearInterval(int3);
                     img.classList.remove("anim");
                     fruits.push(icons[index3]);
                     calculate(fruits);
@@ -145,15 +144,16 @@
         else if (fruit1 == fruit2 || fruit1 == fruit3 || fruit2 == fruit3) {
             if (!autorun) output("Double Fruit!!", 1);
             else feedback = "Double Fruit!!";
-            gain = 4 * bet;
+            gain = 3 * bet;
         }
         else feedback = quips[giveRandom(quips)];
 
         // transaction
 
         balance += gain;
+        console.log(`Spin: ${fruit1} + ${fruit2} + ${fruit3}`)
 
-        setTimeout(() => {if (autorun && balance !> 1){spin()} else {spinning = false; if (autorun) autorun = false}}, 500)
+        setTimeout(() => {if (autorun && balance !> 1){spin()} else {spinning = false; if (autorun) autorun = false}}, 700)
     }
 
     function output(feed:string, type:number) {
@@ -168,7 +168,6 @@
                     setTimeout(() => {
                         clearInterval(int4);
                         feedback = feed;
-                        check();
                     }, 1000)
                 }, 100);
                 break;
@@ -183,7 +182,6 @@
                     setTimeout(() => {
                         clearInterval(int3);
                         feedback = feed;
-                        check();
                     }, 1000)
                 }, 200);
                 break;
@@ -197,7 +195,6 @@
                     setTimeout(() => {
                         clearInterval(int2);
                         feedback = feed;
-                        check();
                     }, 1000)
                 }, 500);
                 break;
@@ -206,17 +203,11 @@
         }
     }
 
-    function check() {
-        if (balance < bet) feedback = "Too much bet!";
-        else if (balance == 0) "Not enough chips to spin!"
-    }
 </script>
 
 <div class="h-screen flex justify-center items-center text-white select-none background overflow-hidden">
-    <div class="border p-2 bg-red-600 sm:rounded-3xl">
-        <div>
-            <h1 class="text-5xl text-center orangek font-bold">Fruit Frenzy</h1>
-        </div>
+    <div class=" pt-2 px-2 bg-red-700 sm:rounded-3xl border">
+        <h1 class="text-5xl text-center orangek font-bold border-b mb-2">Fruit Frenzy</h1>
         <div class="border flex flex-row items-center mb-2 select-none">
             {#each [slot1, slot2, slot3] as item,i}
             <div id={"spin"+(i+1)} class="border bg-gradient-to-b from-slate-300 from-10% via-white via-50% to-slate-300 to-90% overflow-clip max-sm:h-[120px] max-sm:w-[120px] sm:w-[150px] sm:h-[150px] md:h-[200px] md:w-[200px] lg:h-[250px] relative lg:w-[250px]">
@@ -224,28 +215,79 @@
             </div>
             {/each}
         </div>
-        <div class="flex flex-col gap-2">
-            <input type="text" disabled class="basis-2/4 text-3xl rounded-2xl sm:hidden text-center orangek pointer-events-none border" bind:value={feedback} class:animate-pulse={spinning}>
+        <div class="flex flex-col gap-2 border-t pt-2">
+            <input type="text" disabled class="basis-2/4 text-3xl rounded-2xl orangek sm:hidden text-center pointer-events-none border bg-red-900 border-red-900" bind:value={feedback}>
             <div class="flex flex-row gap-2 orangek select-none">
-                <button type="button" class="border basis-1/4 text-3xl rounded-2xl max-sm:basis-1/2 disabled:bg-black hover:scale-[1.05] disabled:hover:scale-100 duration-300" class:animate-pulse={spinning} disabled={spinning || !bet || bet < 0 || balance < bet} onclick={spin}>Spin</button>
-                <input type="text" disabled class="basis-2/4 text-3xl rounded-2xl max-sm:hidden text-center pointer-events-none border" bind:value={feedback} class:animate-pulse={spinning}>
-                <button type="button" class=" border basis-1/4 text-3xl rounded-2xl animation max-sm:basis-1/2 hover:scale-[1.05] duration-300" class:animate-pulse={autorun} disabled={!bet || bet < 0 || balance < bet} onclick={() => {autorun = !autorun; if (!spinning) spin();}}>{#if autorun}Autorun On{:else}Autorun{/if}</button>
+                <button class:bg-red-500={!spinning} type="button" class="border border-red-500 basis-1/4 text-3xl rounded-2xl max-sm:basis-1/2 disabled:bg-transparent hover:scale-[1.05] disabled:hover:scale-100 duration-200" class:animate-pulse={spinning} disabled={spinning || !bet || bet < 0 || balance < bet} onclick={spin}>Spin</button>
+                <input type="text" disabled class="basis-2/4 text-3xl rounded-2xl max-sm:hidden text-center pointer-events-none border bg-red-900 border-red-900" bind:value={feedback}>
+                <button class:bg-red-500={!autorun} type="button" class="border-red-500 disabled:bg-transparent disabled:hover:scale-100 border basis-1/4 text-3xl rounded-2xl animation max-sm:basis-1/2 hover:scale-[1.05] duration-200" class:animate-pulse={autorun} disabled={!bet || bet < 0 || balance < bet} onclick={() => {autorun = !autorun; if (!spinning) spin();}}>{#if autorun}Autorun On{:else}Autorun{/if}</button>
             </div>
-            <div class="flex flex-row max-lg:flex-col gap-1">
+            <div class="flex flex-row max-lg:flex-col gap-2">
                 <div class="flex basis-1/2">
-                    <span class="inline-flex items-center text-2xl  bg-red-300 basis-1/4 border rounded-e-0  border-e-0 rounded-s-2xl justify-center orangek">BET:</span>
+                    <span class="inline-flex items-center text-2xl  bg-red-900 border-red-900 basis-1/4 border rounded-e-0  border-e-0 rounded-s-2xl justify-center orangek">BET:</span>
                     <input type="number" min="1" max={balance} disabled={spinning} bind:value={bet} class="rounded-none basis-3/4 rounded-e-2xl bg-red-50 border text-gray-900 py-2 pe-1 text-center" placeholder="Place your bet!">
                 </div>
                 <div class="basis-1/2">
                     <div class="flex">
-                        <span class="inline-flex items-center text-2xl px-1 bg-red-300 border rounded-e-0 border-e-0 rounded-s-2xl basis-3/12 justify-center orangek">BALANCE:</span>
+                        <span class="inline-flex items-center text-2xl px-1 bg-red-900 border-red-900 border rounded-e-0 border-e-0 rounded-s-2xl basis-3/12 justify-center orangek">BALANCE:</span>
                         <input type="text" min="1" disabled bind:value={balance} class="rounded-none bg-red-50 border border-e-0 text-gray-900 py-2 text-center basis-[63%]" placeholder="Place your bet!">
                         <span class="inline-flex items-center text-2xl px-1 bg-red-50 border border-s-0 rounded-e-2xl basis-[12%] justify-end orangek"><img src={chip} alt="chip" class="h-8"></span>
                     </div>
                 </div>
             </div>
             
-        <span class="block text-sm text-center border-t text-wrap">Betropolis Casino</span>
+        <button class="text-center border-t text-wrap hover:underline max-sm:underline orangek text-xl" onclick={() => modal = true}>Rules</button>
+        </div>
+    </div>
+</div>
+
+<div class:hidden={!modal} class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+
+    <div class="fixed inset-0 bg-black/90 transition-opacity" aria-hidden="true"></div>
+    
+    <div class="fixed inset-0 z-10 w-screen overflow-y-auto text-white">
+        <div class="flex min-h-full justify-center p-4 text-center items-center sm:p-0">
+
+            <div  class="shadow-[0_0px_600px_-15px_rgba(0,0,0,0.3)] shadow-white relative transform overflow-hidden bg-red-700 text-left border transition-all sm:my-8 sm:w-full sm:max-w-2xl mx-2 p-2 rounded-3xl orangek">
+                <h1 class="text-center text-5xl pb-2 border-b">Rules</h1>
+                <p class="text-justify m-1 text-xl sm:text-2xl leading-6">The reels contain 7 fruits and a golden variant of one of the fruits. Press the spin button to try your luck. You can use the autorun button to keep the machine continously running. The minimum bet is 1 chip.</p>
+                <table class="mx-auto text-lg sm:text-2xl border rounded my-2 [&_th]:border [&_td]:border [&_td]:p-2">
+                    <thead>
+                        <tr class="text-center">
+                            <th>Name</th>
+                            <th>Fruits</th>
+                            <th>Win</th>
+                        </tr>
+                    </thead>
+                    <tbody class="">
+                        <tr>
+                            <td>Double Fruit</td>
+                            <td>2 identical fruits</td>
+                            <td>2x + your bet</td>
+                        </tr>
+                        <tr class="bg-yellow-400/60">
+                            <td>Double Golden Fruit</td>
+                            <td>2 golden fruits</td>
+                            <td>10x + your bet</td>
+                        </tr>
+                        <tr>
+                            <td>Fruit Frenzy</td>
+                            <td>3 identical fruits</td>
+                            <td>100x + your bet</td>
+                        </tr>
+                        <tr class="bg-yellow-400/60">
+                            <td>Golden Fruit Frenzy</td>
+                            <td>3 golden fruits</td>
+                            <td>500x + your bet</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div class="border-t pt-2 flex justify-center">
+                    <button class="border text-2xl p-1 rounded-2xl w-24" onclick={() => modal = false}>Close</button>
+                </div>
+            </div>
+
+            
         </div>
     </div>
 </div>
