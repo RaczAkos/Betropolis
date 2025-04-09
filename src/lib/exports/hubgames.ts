527d7877033e8aecfa9fd92de6dd3b335d0db432
RaczAkos <akocska2005@gmail.com>
2025-04-09 23:21:01 +0200

    hub carousel fix

--- src/lib/exports/hubgames.ts
+++ src/lib/exports/hubgames.ts
@@ -2,6 +2,12 @@ import crash from '$lib/media/images/hub/dragoncrash.png'
 import slot from '$lib/media/images/hub/fruit-frenzy.png'
 import findcard from '$lib/media/images/hub/findcard.png'
 
-let images:string[] = [crash, slot, findcard];
+let englishImages:string[] = [crash, slot, findcard];
 
-export default images;
+import hunCrash from '$lib/media/images/hub/hungarian-crash.png'
+import hunSlot from '$lib/media/images/hub/hungarian-fruit-frenzy.png'
+import hunFindcard from '$lib/media/images/hub/hungarian-findcard.png'
+
+let hungarianImages:string[] = [hunCrash, hunSlot, hunFindcard];
+
+export default [englishImages, hungarianImages];
